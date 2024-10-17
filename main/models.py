ba3565387667b4a4db9c2ffd01f7c4801bd81dbf
diimartell <ruanvitoraquinooliveira@gmail.com>
2024-10-17 20:57:50 -0300

    Api
    Aula sobre apis

---
+++ main/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class item(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    valor = models.FloatField()
+    
+    def __str__(self):
+        return self.nome
+    
